4bae939e024b1a01aa654c50ee6af3f002d39c93
jenna <ravennasw@gmail.com>
2022-12-03 20:25:38 +0700

    change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
         int price = 12000;
         int oneMile = 20;
         int bonusMiles = price / oneMile * 1;
 
         System.out.println("Начислено бонусных миль: " + bonusMiles);
-
-
     }
 }
