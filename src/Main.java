303c9b0f1521ce933a0dcb4b4ab681fbb810fec3
jenna <ravennasw@gmail.com>
2022-12-03 17:58:52 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 12000;
+        int oneMile = 20;
+        int bonusMiles = price / oneMile * 1;
+
+        System.out.println("Начислено бонусных миль: " + bonusMiles);
+
+
+    }
+}
